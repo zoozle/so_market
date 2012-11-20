a0dc01e0bb61f0fd05636e187b20d2026a6149ab
Zoozle <prunnerty@gmail.com>
2012-11-20 05:26:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoMarket::Application.config.secret_token = '1b3b9a984d4c71831728aea7012fbfc07c5e1533436dd456147d26f8a86b14993ad0d4e9110905f1522926a888dc8c987d2579790422ccdd9dd327e19378d0aa'
